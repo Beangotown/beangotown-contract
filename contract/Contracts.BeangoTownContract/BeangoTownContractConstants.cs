5e4fe05f5afab6df547b4fa5bbef7a96aa64e7f5
louis <louis@beangotown.com>
2023-09-11 15:17:58 +0800

    feat: update DailyMaxPlayCount

--- contract/Contracts.BeangoTownContract/BeangoTownContractConstants.cs
+++ contract/Contracts.BeangoTownContract/BeangoTownContractConstants.cs
@@ -4,7 +4,7 @@ public class BeangoTownContractConstants
 {
     public const string BeanPassSymbol = "BEANPASS-1";
 
-    public const int DailyMaxPlayCount = 5;
+    public const int DailyMaxPlayCount = 20;
     public const int DailyPlayCountResetHours = 0;
     public const int BlueGridScore = 1;
     public const int RedGridScore = 5;
