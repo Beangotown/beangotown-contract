41754d0c7c3f447413b43b5cf9884b58c83d3564
louis <louis@beangotown.com>
2024-01-08 16:39:36 +0800

    feat: add doc

--- contract/Contracts.BeangoTownContract/BeangoTownContractState.cs
+++ contract/Contracts.BeangoTownContract/BeangoTownContractState.cs
@@ -10,13 +10,15 @@ public partial class BeangoTownContractState : ContractState
 {
     // state definitions go here.
     public BoolState Initialized { get; set; }
-
     public SingletonState<Address> Admin { get; set; }
     public SingletonState<GameLimitSettings> GameLimitSettings { get; set; }
     public SingletonState<GameRules> GameRules { get; set; }
     public SingletonState<GridTypeList> GridTypeList { get; set; }
+
+    // PlayerAddress => PlayerInformation
     public MappedState<Address, PlayerInformation> PlayerInformation { get; set; }
 
+    // PlayId => BoutInformation
     public MappedState<Hash, BoutInformation> BoutInformation { get; set; }
    
 }
